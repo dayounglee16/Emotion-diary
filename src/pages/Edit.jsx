62971c540825a3c9e09b8f2148b61c162d16884b
이다영 <id46827@gmail.com>
2024-08-31 08:45:29 +0900

    일기 삭제 기능구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
+  const nav = useNavigate();
   const params = useParams();
-  return <div>{params.id}번 일기입니다</div>;
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(params.id);
+      nav("/", { replace: true }); //replace는 특정 기능 후 뒤로가기 방지
+    }
+  };
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button text={"< 뒤로가기"} onClick={() => nav(-1)} />}
+        rightChild={
+          <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
