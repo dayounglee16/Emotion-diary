564e958b742963c53cfba4d6c39d4401dec237fd
이다영 <id46827@gmail.com>
2024-08-30 19:52:10 +0900

    일기 추가, 일기 수정, 일기 삭제 기능구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,92 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { useReducer, useRef, createContext } from "react";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+  }
+};
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
-  return <></>;
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  //새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  //기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: idRef.current++,
+      },
+    });
+  };
+
+  //기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: {
+        id,
+      },
+    });
+  };
+  return (
+    <>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={(onCreate, onUpdate, onDelete)}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/edit/:id" element={<Edit />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
 }
 
 export default App;
