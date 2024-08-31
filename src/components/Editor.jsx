cdbf4146e36a3e0b01da0970ac114ba844478804
이다영 <id46827@gmail.com>
2024-08-31 22:30:38 +0900

    Diary페이지 UI 및 기능구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,43 +3,8 @@ import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-const emotionList = [
-  {
-    emotionId: 1,
-    emotionName: "완전 좋음",
-  },
-  {
-    emotionId: 2,
-    emotionName: "좋음",
-  },
-  {
-    emotionId: 3,
-    emotionName: "그럭저럭",
-  },
-  {
-    emotionId: 4,
-    emotionName: "나쁨",
-  },
-  {
-    emotionId: 5,
-    emotionName: "끔찍함",
-  },
-];
-
-const getStringedDate = (targetDate) => {
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (date < 10) {
-    date = `0${month}`;
-  }
-  return `${year}-${month}-${date}`;
-};
+import { emotionList } from "../constants";
+import { getStringedDate } from "../util/getStringedDate";
 
 const Editor = ({ onSubmit, initData }) => {
   const [input, setInput] = useState({
